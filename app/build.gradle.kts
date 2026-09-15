ac996e40edd72a2f8a1b96cd9f9192518ef9e08c
ishfaque123 <ishfaqshar70@gmail.com>
2026-09-15 19:41:17 +0500

    Add native Google Credential Manager dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.credentials:credentials:1.7.0-alpha03")
+    implementation("androidx.credentials:credentials-play-services-auth:1.7.0-alpha03")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.2.0")
 }
