1ecf1f01986181b663a141e335d1ed2989e3e016
ishfaque123 <ishfaqshar70@gmail.com>
2026-09-17 11:12:58 +0500

    Update Google Identity SDK to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,5 +60,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.credentials:credentials:1.7.0-alpha03")
     implementation("androidx.credentials:credentials-play-services-auth:1.7.0-alpha03")
-    implementation("com.google.android.libraries.identity.googleid:googleid:1.2.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.2.1")
 }
