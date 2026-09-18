45d243e0dd54d9f3dc3a10be723e8cacad49d1a9
ishfaque123 <ishfaqshar70@gmail.com>
2026-09-18 14:17:43 +0500

    Sign debug APK with release keystore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("release")
+        }
         release {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
