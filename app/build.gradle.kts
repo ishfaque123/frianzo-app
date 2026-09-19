45a831837113ccc5ef8b797fd63e783a1d56ed4a
ishfaque123 <ishfaqshar70@gmail.com>
2026-09-19 20:41:13 +0500

    Bump Android versionCode from 1 to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.frianzo.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
     }
 
